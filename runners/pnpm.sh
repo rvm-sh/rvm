c238629a2745b9cca7b6165444567e4e48301f90
Chironjit Das <chironjit@gmail.com>
2024-02-02 14:42:09 +0100

    pnpm file wip

--- runners/pnpm.sh
+++ runners/pnpm.sh
@@ -1,46 +1,50 @@
 #!/bin/sh
 
+# Define a list of installed pnpm versions
+installed_versions="8.14.0 8.14.3 8.15.0 8.15.1"
+
 # Placeholder for latest and default versions of pnpm
 LATEST_PNPM_VERSION="8.15.1"
-DEFAULT_PNPM_VERSION="8.15.1"
+DEFAULT_PNPM_VERSION="8.14.3"
 
 # Function to handle pnpm commands
 pnpm() {
-    echo "Debug: Running pnpm version $version_to_use at $pnpm_executable"
-
-    # Execute the pnpm command with the specified version
-    "$pnpm_executable" "$@"
-
-    
-    local full_command=$1
     local specified_version=""
-    local actual_command=""
+    local version_to_use=""
+    local version_command="$1"
+    shift  # Shift here to correctly process remaining arguments
 
-    # Check if a specific version is requested
-    if echo "$full_command" | grep -q "@"; then
-        specified_version=$(echo "$full_command" | cut -d'@' -f2)
-        actual_command=$(echo "$full_command" | cut -d'@' -f1)
+    # Extracting the version if specified
+    if echo "$version_command" | grep -q "@"; then
+        specified_version=$(echo "$version_command" | cut -d'@' -f2)
+        # Check if specified version is in the list of installed versions
+        if echo "$installed_versions" | grep -qw "$specified_version"; then
+            version_to_use=$specified_version
+        else
+            echo "Specified version $specified_version is not installed."
+            return 1
+        fi
+    elif [ "$version_command" = "pnpm@latest" ]; then
+        version_to_use=$LATEST_PNPM_VERSION
     else
-        specified_version=$DEFAULT_PNPM_VERSION
-        actual_command=$full_command
+        version_to_use=$DEFAULT_PNPM_VERSION
+        set -- "$version_command" "$@"  # Prepend version_command back to arguments if not specifying a version
     fi
 
-    shift  # Remove the first argument
-
-    # Determine which version to use
-    local version_to_use=${specified_version:-$DEFAULT_PNPM_VERSION}
-
-    # Path to the pnpm executable for the chosen version
-    local pnpm_executable="${HOME}/.pnpm/${version_to_use}/pnpm"
+    local pnpm_executable="${HOME}/.pnpm/v${version_to_use}/pnpm"
+    echo "Selected version: $version_to_use"
+    echo "Executing: $pnpm_executable with arguments: $@"
 
     if [ ! -f "$pnpm_executable" ]; then
-        echo "pnpm version $version_to_use not found."
+        echo "Executable for pnpm version $version_to_use not found."
         return 1
     fi
 
     # Execute the pnpm command with the specified version
-    "$pnpm_executable" "$actual_command" "$@"
+    $pnpm_executable "$@"
 }
+
+
 
 
 #when adding an uninstall all function, remember to remove also the shared folder in ./local/share that stores teh shared packages/modules
