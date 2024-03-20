938e00d09d37c29f22214739ef7ba1e0fdbaf80a
Chironjit Das <chironjit@gmail.com>
2024-03-20 17:59:51 +0100

    Completed help function for all runtime management function calls

--- templates/runtime_template.sh
+++ templates/runtime_template.sh
@@ -16,13 +16,11 @@ add () {
 }
 
 help_add () {
-    echo "Add installs either <specific version>, <latest>, <major_version> or <major_minor_version> depending on availability by runtime managers"
+    echo "Add installs either <specific version>, <latest>, <major_version> depending on availability by runtime managers"
     echo "rvm add $RUNTIME <version> - installs the specific version of $RUNTIME. eg:"
     echo "rvm add $RUNTIME latest - installs the latest version of $RUNTIME"
     echo "rvm add $RUNTIME 8 - installs the latest version of $RUNTIME 8"
-    echo "rvm add $RUNTIME 8.14 - installs the latest version of $RUNTIME 8.14"
     echo "rvm add $RUNTIME 8.14.12 - installs this specific version of $RUNTIME 8.14.12"
-
 }
 
 # Remove removes <specific_version>, <major_version> and <major> versions only currently
@@ -33,12 +31,24 @@ remove () {
 
 }
 
+help_remove()  {
+    echo "Remove removes <specific_version> and <major_version> versions only currently"
+    echo "rvm remove $RUNTIME <version> - removes the specific version of $RUNTIME. eg:"
+    echo "rvm remove $RUNTIME 8         - removes the latest version of $RUNTIME 8"
+    echo "rvm remove $RUNTIME 8.14.12   - removes this specific version of $RUNTIME 8.14.12"
+}
+
 # Update installs latest of the major version being used and makes it the default
 # rvm update node
 # will update to the latest node 18 if that is the major version set as default 
 
 update () {
 
+}
+
+help_update() {
+    echo "Update installs latest of the major version being used and makes it the default"
+    echo "rvm update $RUNTIME - e.g updates to the latest version of $RUNTIME v8 if that is the major version set as default"
 }
 
 # Upgrade adds the latest version and makes it the default
@@ -48,11 +58,21 @@ upgrade () {
 
 }
 
+help_upgrade() {
+    echo "Upgrade installs the latest version of $RUNTIME and makes it the default. Unlike the update command, it will jump major versions if there is a newer major version available"
+    echo "rvm update $RUNTIME"
+}
+
 # Removeall removes all the versions of the runtime 
 # rvm removeall node
 
 removeall() {
 
+}
+
+help_removeall() {
+    echo "Removeall removes all the versions of $RUNTIME added to system. eg."
+    echo "rvm removeall $RUNTIME"
 }
 
 # Use sets the specific <major> or <major_minor_rev> version of the runtime temporarily. Resets on restart
@@ -63,6 +83,13 @@ use () {
 
 }
 
+help_use() {
+    echo "Use sets the specific <major> or <major_minor_rev> version of the runtime temporarily. Resets on restart of os. eg."
+    echo "rvm use $RUNTIME <version>"
+    echo "rvm use $RUNTIME 18       - will use the latest version of $RUNTIME 18"
+    echo "rvm use $RUNTIME 18.14.12 - will set the specific version(18.14.12) of $RUNTIME requested"
+}
+
 # Set sets the specified <major> or <major_minor_rev> version as the default version
 # rvm set node 18
 # rvm set node 18.14.12
@@ -70,11 +97,28 @@ set () {
 
 }
 
+help_set() {
+    echo "Set sets the specified <major> or <major_minor_rev> version as the default version. eg."
+    echo "rvm set $RUNTIME <version>"
+    echo "rvm set $RUNTIME 18       - will set the latest version of $RUNTIME 18 as the default"
+    echo "rvm set $RUNTIME 18.14.12 - will set the specific version(18.14.12) of $RUNTIME requested as the default"
+}
+
 # Help displays the generic help for 
 # rvm help node
 
 help() {
+    echo "Welcome to the help section for $RUNTIME management via rvm"
+    echo "rvm supports the following commands for managing the $RUNTIME runtime: add, remove, update, upgrade, removeall, showall, use, set, help, supported, prune"
+    echo "rvm <command> $RUNTIME help - displays the help for the specific command. e.g:"
+    echo "rvm add $RUNTIME help - displays the help for the add command"
+    echo "rvm remove $RUNTIME help - displays the help for the remove command"
 
+    echo "To run a command, use the following syntax:"
+    echo "rvm supported $RUNTIME -  returns positive confirmation that $RUNTIME is supported"
+    echo "rvm add $RUNTIME latest - installs the latest version of the runtime, as define by the runtime maintainers"
+    echo "rvm showall $RUNTIME installed - lists all installed versions of the runtime"
+    echo "rvm showall $RUNTIME available - lists all available versions of the runtime from the runtime repository"
 }
 
 # Returns positive confirmation that runtime is supported
@@ -82,6 +126,13 @@ help() {
 supported() {
 
 }
+
+supported_help() {
+    echo "Supported returns positive confirmation that $RUNTIME is supported. e.g.:"
+    echo "rvm supported $RUNTIME"
+}
+
+
 
 
 # Showall <installed> shows all installed versions of runtime
@@ -92,12 +143,26 @@ showall() {
 
 }
 
+help_showall() {
+    echo "Showall shows all installed or available versions of runtime depending on the command option"
+    echo "rvm showall $RUNTIME installed - lists all installed versions of $RUNTIME"
+    echo "rvm showall $RUNTIME available - lists all available versions of $RUNTIME from the runtime repository"
+}
+
 # Prune all versions of runtime older than the stated version
 # rvm prune node 18
 # rvm prune node 18.10
 # rvm prune node 18.10.14 
 prune() {
     
+}
+
+help_prune() {
+    echo "Prune all versions of $RUNTIME older than the stated version"
+    echo "rvm prune $RUNTIME <version>  - removes all versions of $RUNTIME older than the stated version. eg:"
+    echo "rvm prune $RUNTIME 18         - removes all versions of $RUNTIME older than the latest version of $RUNTIME 18 (not including any versions 18)"
+    echo "rvm prune $RUNTIME 18.10      - removes all versions of $RUNTIME older than the latest version of $RUNTIME 18.10 (not including version 18.10)"
+    echo "rvm prune $RUNTIME 18.10.14   - removes all versions of $RUNTIME older than the latest version of $RUNTIME 18.10.14 (not including version 18.10.14)"
 }
 
 
